0c8dd77aa36f0c0e786be57e01333d6aa640d4be
leevilukas <95694522+leevilukas@users.noreply.github.com>
2021-12-15 23:39:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+import Deck from "./deck.js"
+
+
+const playerCardSlot = document.querySelector('.player-card-slot')
+const deck = new Deck()
+deck.shuffle()
+console.log(deck.cards)
+
+
+
+playerCardSlot.appendChild(deck.cards[0].getHTML())
+
+
+
+
+
+
